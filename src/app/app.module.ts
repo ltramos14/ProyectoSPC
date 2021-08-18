949495a0ec87e18400c2701482692e43f3975a2e
Tatiana Villanueva <tatianarv14@gmail.com>
2021-08-18 17:59:41 -0500

    Creacion de componentes y modulos iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+import { SpcModule } from './pages/spc/spc.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AuthModule,
+    SharedModule,
+    SpcModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/spc/spc.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { UsersComponent } from './users/users.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent, 
+    ProductsComponent, 
+    ProductDetailsComponent, 
+    ShoppingCartComponent, UsersComponent, ContactUsComponent, AboutUsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent, 
+    ProductsComponent, 
+    ProductDetailsComponent, 
+    ShoppingCartComponent
+  ]
+})
+export class SpcModule { }
