e0ba80c124113cd8466bf7ae6829cc57075b65be
Nicolas Nieto <niconieto1506@gmail.com>
2021-09-11 22:02:50 -0500

    Diseño pagina de error

--- src/app/pages/error/page-not-found/page-not-found.component.ts
+++ src/app/pages/error/page-not-found/page-not-found.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import icSearch from '@iconify/icons-ic/twotone-search';
 
 @Component({
   selector: 'vex-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent implements OnInit {
+
+  icSearch = icSearch;
 
   constructor() { }
 
