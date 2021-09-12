666d09c217ba0f933859d1c5b8757288e15d91ba
Nicolas Nieto <niconieto1506@gmail.com>
2021-09-11 23:56:59 -0500

    Registro diseño

--- src/app/pages/auth/forgot-password/forgot-password.module.ts
+++ src/app/pages/auth/forgot-password/forgot-password.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
-
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { IconModule } from '@visurel/iconify-angular';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     ForgotPasswordComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    IconModule,
+    MatIconModule
   ]
 })
 export class ForgotPasswordModule { }
