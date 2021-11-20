9ea55cf5505e2edc3fdd6818e6c6e4c9442396e6
Johann Nicolás Nieto Cárdenas <48180657+jnnieto@users.noreply.github.com>
2021-11-20 01:37:57 -0500

    service de fincas y actualizacion de imagen de usuario

---
+++ src/app/service/producer/farms.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { User } from "src/app/interfaces/user.interface";
+import { Farm } from "src/app/models/farm.model";
+import { AuthService } from "../auth/auth.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FarmsService {
+
+  /**
+   * Variable que instancia la colección de las fincas del productor
+   */
+  farmsCollecition: AngularFirestoreCollection<Farm>;
+
+  /**
+   * Variable de tipo Observable que contiene la lista de fincas vinculados al productor
+   */
+  farms: Observable<Farm[]>;
+
+  /**
+   * Variable de tipo string que almacena el id del productor para indicar la ruta de la colección
+   */
+  producerDoc: AngularFirestoreDocument<User>;
+
+  /**
+   * Constructor de FarmsService
+   * @param afs 
+   * @param authService 
+   */
+  constructor(public afs: AngularFirestore, private authService: AuthService) {
+    this.authService.getCurrentUser().then((data) => {
+    this.producerDoc = afs.doc<User>(`users/${data.uid}`);
+    this.farmsCollecition = this.producerDoc.collection<Farm>('farms');
+    this.getFarms();
+  });
+  }
+
+  /**
+   * Método que obtiene los documentos de la colección de Fincas del productor
+   */
+  getFarms() {
+    this.farms = this.farmsCollecition.snapshotChanges()
+      .pipe(map((actions) => actions.map((a) => a.payload.doc.data() as Farm)));
+  }
+
+  /**
+   * Método que actualiza o edita la información de una fica en la subcolección 'farms' de 'users'
+   * @param idFarm id en la finca en caso de que edite, si crea este valor llega null
+   * @param farm el objeto 
+   * @returns 
+   */
+  saveFarm(idFarm: string, farm: Farm): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const id = idFarm || this.afs.createId();
+        const data = { id, ...farm };
+        const result = await this.farmsCollecition.doc(id).set(data);
+        resolve(result);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  /**
+   * Método que elimina la finca de un productor asociada a su perfil
+   * @param idFarm 
+   * @returns 
+   */
+  deletePaymentMethod(idFarm: string): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await this.farmsCollecition
+          .doc(idFarm)
+          .delete();
+        resolve(result);
+      } catch (err) {
+        reject(err.message);
+      }
+    });
+  }
+
+}
