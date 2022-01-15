0946d586c074e0cb581d07b481eda33e6ce2b169
ltramos14 <tatianarv14@gmail.com>
2022-01-14 22:09:50 -0500

    Cambios en la pagina inicial y se crea el footer

---
+++ src/app/pages/spc/shared/footer/footer.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { IconModule } from '@visurel/iconify-angular';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [
+  CommonModule,
+    FlexLayoutModule,
+    IconModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports: [FooterComponent]
+})
+export class FooterModule { }
